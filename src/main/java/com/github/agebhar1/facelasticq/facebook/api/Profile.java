ea6f43e4a031cdd8cb121c84f28d0cb002a49f87
Andreas Gebhardt <agebhar1@googlemail.com>
2016-08-13 15:13:18 +0200

    add value object for Facebook `Profile` v2.7
    Facebook's `Profile` [1] (v2.7) is a generic type. All
    types have attribute `id` and `name` in common. A profile
    is on off:
    
      * User [2]
      * Page [3]
      * Group [4]
      * Event [5]
      * Application [6]
    
    The value object consists of the attributes `id` and
    `name` all other properties are keept as key/value
    pairs within `*JsonProperties` map.
    
    [1] https://developers.facebook.com/docs/graph-api/reference/v2.7/profile/
    [2] https://developers.facebook.com/docs/graph-api/reference/user
    [3] https://developers.facebook.com/docs/graph-api/reference/page
    [4] https://developers.facebook.com/docs/graph-api/reference/group
    [5] https://developers.facebook.com/docs/graph-api/reference/event
    [6] https://developers.facebook.com/docs/graph-api/reference/app

---
+++ src/main/java/com/github/agebhar1/facelasticq/facebook/api/Profile.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2016 Andreas Gebhardt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.github.agebhar1.facelasticq.facebook.api;
+
+import java.util.Map;
+
+import lombok.Builder;
+import lombok.Singular;
+import lombok.Value;
+
+/**
+ * 
+ * A lightweight model for a Facebook profile object.
+ * 
+ * @see https://developers.facebook.com/docs/graph-api/reference/v2.7/profile/
+ *
+ * A profile can be a:
+ *  * User
+ *  * Page
+ *  * Group
+ *  * Event
+ *  * Application
+ *
+ */
+
+@Value
+@Builder
+public class Profile {
+	
+	/**
+	 * The profile ID, each of User, Page, Group
+	 * Event or Application has one.
+	 */
+	String id;
+	
+	/**
+	 * The name of either User, Page, Group,
+	 * Event or Application.
+	 */
+	String name;
+	
+	/**
+	 * anything else
+	 */
+	@Singular Map<String, Object> jsonProperties;	
+
+}
